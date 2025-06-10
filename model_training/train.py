2f6a019e4f728dac23d4b5dab2709c7a3962eb3a
Joseph Ghastin <jmghastin1@gmail.com>
2025-06-10 08:47:22 -0400

    add new branch

--- model_training/train.py
+++ model_training/train.py
@@ -1,3 +1,8 @@
-from transformers import AutoModelForSeq2SeqLM
+#!/usr/bin/env python3
 
+from transformers import AutoModelForSeq2SeqLM, trainer, PreTrainedTokenizer
+
+#model to be trained
 model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-es")
+
+
