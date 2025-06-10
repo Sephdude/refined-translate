ff38d02cf0f2dcfec8230dc1c72eb8c47740c32a
Joseph Ghastin <jmghastin1@gmail.com>
2025-06-10 13:25:20 -0400

    add model training support

--- model_training/train.py
+++ model_training/train.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python3
 
 from datasets import load_dataset, Dataset
-from transformers import TrainingArguments, AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import TrainingArguments, AutoTokenizer, AutoModelForSeq2SeqLM, Trainer, DataCollatorForSeq2Seq
 import numpy as np
 import evaluate
 
+
 #model to be trained
-odel = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-es")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-es")
 
-
-#data set from which to be trained
-ds = load_dataset("Helsinki-NLP/opus_fiskmo")
-tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-es")
 
 #tokenize
-ds = Dataset.from_dict(ds)
+dataset = load_dataset("Nicolas-BZRD/English_French_Webpages_Scraped_Translated")
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-es")
+print(dataset.keys())
+#def tokenize(examples):
+#    return tokenizer(examples["en"], examples['fr'], padding="max_length", truncation=True)
 
-def tokenize(data):
-    fi_tokens = tokenizer(data["fi"], padding="max_length", truncation=True)
-    sv_tokens = tokenizer(data["sv"], padding="max_length", truncation=True)
-    return {"en_tokens": fi_tokens, "es_tokens": sv_tokens}
+def tokenize(examples):
+    # tokenize source sentences (English)
+    inputs = tokenizer(examples["en"], max_length=128, truncation=True)
+    # tokenize target sentences (French)
+    targets = tokenizer(examples["fr"], max_length=128, truncation=True)
+    # set labels for decoder - typical Marian fine-tuning
+    inputs["labels"] = targets["input_ids"]
+    return inputs
 
-ds = ds.tokenize(ds)
+
+
+
+dataset = dataset["train"].select(range(1000)).map(tokenize, batched=True)
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+
 
 #set up compute metrics
 def compute_metrics(eval_pred):
@@ -34,16 +44,17 @@ def compute_metrics(eval_pred):
 #Training arguments
 training_args = TrainingArguments(
     output_dir="model_output", #output folder
-    eval_strategy="epoch"
+    #eval_strategy="epoch"
 )
 
 #trainer
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=dataset["sv"],
-    eval_dataset=dataset["fi"],
+    train_dataset=dataset,
+    #eval_dataset=dataset["test"],
     compute_metrics=compute_metrics,
+    data_collator=data_collator,
 )
 print('hi')
-#trainer.train()
+trainer.train()
