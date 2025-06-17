040c0471320a01cde182670ee538f8c9a8d0ff44
Joseph Ghastin <jmghastin1@gmail.com>
2025-06-17 09:59:53 -0400

    manage to train stuff

---
+++ data/format_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+#scripts to transform data into a hugging face compatible datasets
+
+#split every sentence up into a list
+def new_line(text_file):
+    
+    #split by periods
+    with open(text_file, 'r') as file:
+        data = file.read()
+        data = data.split('.')
+
+    #remove single periods or extraneous symbols
+    data = [sentence.strip() for sentence in data if sentence.strip() != '']
+
+    print(data)
+
+new_line("resources-PR/articles.txt")    
+
