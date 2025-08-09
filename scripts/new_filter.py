f81edd5dfa0f402ba488e9058ca324d97be10b98
Joseph Ghastin <jmghastin1@gmail.com>
2025-08-09 19:09:23 -0400

    stopped using re and cleaned up new_filter to make it considerably more efficient

--- scripts/new_filter.py
+++ scripts/new_filter.py
@@ -56,6 +56,11 @@ def filter_words(block):
 
     #extract the Puerto Rican sentences
     for example, metadata in zip(block["text"], block["metadata"]):
+
+        #strip accents and special characters from the text
+        clean_slang = [strip_accents(word.lower()) for word in puerto_rican_slang]
+
+                    
         for text_dict in example:
             
             if data_type == "domain":
@@ -67,17 +72,13 @@ def filter_words(block):
             if data_type == "slang":
                 #only append if there are two Puerto Rican slang words in the sample
                 matched_words = set()
-                for word in puerto_rican_slang:
-                    #strip accents and special characters from the text
-                    pattern = r'\b' + strip_accents(re.escape(word)).replace(r'\ ', r'\s+') + r'\b'
-                    print(pattern)
 
-                    #strip accents from the text to avoid false positives
-                    stripped_text = strip_accents(text_dict["text"])
-                    print(stripped_text)
-                    
+                #strip accents from the text to avoid false positives
+                stripped_text = strip_accents(text_dict["text"]).lower()
+
+                for word in clean_slang:    
                     #check if the word is in the text if the same word has not been already found
-                    if re.search(pattern, stripped_text, re.IGNORECASE):
+                    if word in stripped_text:
                         if word not in matched_words:
                             matched_words.add(word)
                 
@@ -145,24 +146,37 @@ if __name__ == "__main__":
         manager = Manager()
         lock = manager.Lock()
 
-        batch_size = 10000  # Size of each batch
+        batch_size = 50000  # Size of each batch
+        dataset_size = 20600000 # Approximate size of the dataset
+
         process_count = os.cpu_count()  # Number of processes to use
 
         #set this to change the type of data to filter
         data_type = "slang"
+        
+        #track how many batches have been done
+        batches_complete = 0
+
+        #format the dataset
+        formatted_ds = dataset_format(ds["train"])
+
 
         #chunk up batches and filter in multiprocessing
-        for batch in make_batch(dataset_format(ds)["train"], batch_size):
-                    
-                    #set up filter arguments
-                    args = []
-                    for i in range(process_count):
-                        start = i * (len(batch) // process_count)
-                        end = start + (len(batch) // process_count)
-                        args.append(batch.select(range(start, end)))
+        for batch in make_batch(formatted_ds, batch_size):
+            
+            #load bar
+            translate.load_bar(batches_complete * batch_size, dataset_size, "Batches complete: \n")
 
-                    print(args)
+            #set up filter arguments
+            args = []
+            for i in range(process_count):
+                start = i * (len(batch) // process_count)
+                end = start + (len(batch) // process_count)
+                args.append(batch.select(range(start, end)))
 
-                    #set up multiprocessing pool
-                    with Pool(process_count) as pool:
-                         pool.map(filter_words, args)
+
+            #set up multiprocessing pool
+            with Pool(process_count) as pool:
+                    pool.map(filter_words, args)
+
+            batches_complete += 1
